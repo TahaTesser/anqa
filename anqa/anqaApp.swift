1b71dea4c3dc84d4ba98e33f6e4dc315fb934ba6
Taha Tesser <tessertaha@gmail.com>
2024-04-28 11:39:15 +0300

    Initial Commit

---
+++ anqa/anqaApp.swift
@@ -0,0 +1,17 @@
+//
+//  anqaApp.swift
+//  anqa
+//
+//  Created by Taha Tesser on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct anqaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
